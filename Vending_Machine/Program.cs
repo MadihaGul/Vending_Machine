596e03d9a6480e24a2e75ef9f5d0b9e419d0f22a
madiha gul <mmadihagull@gmail.com>
2021-10-21 17:17:05 +0200

    Add project files.

---
+++ Vending_Machine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Vending_Machine.Models;
+namespace Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"+ uint.MaxValue, uint.MinValue);
+
+         
+        }
+    }
+}
